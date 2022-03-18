70c79d516dfe5eaa00287fa3603b4b26bb6cd62f
awalrizqifr <awalrizqifr@gmail.com>
2022-03-18 18:51:16 +0700

    remove /

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::apiResource('/books', BookController::class)->except('show');
+Route::apiResource('books', BookController::class)->except('show');
